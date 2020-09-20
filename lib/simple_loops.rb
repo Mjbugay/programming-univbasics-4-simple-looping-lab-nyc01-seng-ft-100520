27aaaa5735024076a1d3ec1d4859034cc16ea61c
Matt Bugay <mjbugay@yahoo.com>
2020-09-20 21:50:58 +0000

    Automatically backed up by Learn

--- lib/simple_loops.rb
+++ lib/simple_loops.rb
@@ -6,10 +6,12 @@ def loop_message_five_times(message)
   end
 end
 
-def loop_message_n_times(string, n)
+def loop_message_n_times(message, limit)
   count = 0 
-  while count < n do
-  
+  while count < limit do
+    puts message
+    count += 1
+  end
 end
 
   
